1008386511f2fd3b9f01db80a3f30dc8294d7b51
tlg-seneca <46732990+tlg-seneca@users.noreply.github.com>
2019-04-30 05:11:15 -0400

    Update Sprite.js

--- assign2/src/components/Pokemon/Sprite.js
+++ assign2/src/components/Pokemon/Sprite.js
@@ -1,10 +1,10 @@
 /*----------------------------------------------------------------
-*	Author:   Tramaine L. Garner
+* Author:   Tramaine L. Garner
 * Email:    tramainegarner@gmail.com
-*	File:     Sprite.js
-*	Written:  30/04/2019
-*	Purpose:  Display individual Pokemon
-*	Notes:		Encountered routing problem; wasn't sure the best logical placement for routes
+* File:     Sprite.js
+* Written:  30/04/2019
+* Purpose:  Display individual Pokemon
+* Notes:	Encountered routing problem; wasn't sure the best logical placement for routes
 *----------------------------------------------------------------*/
 
 import React, { Component } from 'react';
